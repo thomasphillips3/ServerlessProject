d02abe7be09db122098cd63ecd5f85bdffe3b44e
Thomas Phillips <thomasphillips3@gmail.com>
2019-03-01 19:45:39 -0800

    configures aws-amplify

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 export default class App extends React.Component {
   render() {
